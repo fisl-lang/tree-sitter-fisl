82b7b8b2cfa25782722144e59b42c750517edb2c
S1mon <jaforsi.kozak@gmail.com>
2025-10-20 06:10:57 +0200

    built

--- src/parser.c
+++ src/parser.c
@@ -1548,7 +1548,7 @@ extern "C" {
 #define TS_PUBLIC __attribute__((visibility("default")))
 #endif
 
-TS_PUBLIC const TSLanguage *tree_sitter_fislr(void) {
+TS_PUBLIC const TSLanguage *tree_sitter_fisl(void) {
   static const TSLanguage language = {
     .abi_version = LANGUAGE_VERSION,
     .symbol_count = SYMBOL_COUNT,
@@ -1573,7 +1573,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_fislr(void) {
     .lex_modes = (const void*)ts_lex_modes,
     .lex_fn = ts_lex,
     .primary_state_ids = ts_primary_state_ids,
-    .name = "fislr",
+    .name = "fisl",
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
