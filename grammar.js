71335f20fe125e102cf95ac8db68d4a2d9145aaf
S1mon <jaforsi.kozak@gmail.com>
2025-10-20 05:25:24 +0200

    wrote grammar

--- grammar.js
+++ grammar.js
@@ -8,10 +8,41 @@
 // @ts-check
 
 module.exports = grammar({
-  name: "fisl",
+  name: "fislr",
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => repeat(seq($.line, "\n")),
+    line: $ => choice($.statement, $.comment),
+    comment: $ => /#.+?\n/,
+    statement: $ => seq(
+      seq("let", $.var, "be", $.expr),
+      seq("let", $.var, "be", $.expr, "plus",  $.expr),
+      seq("let", $.var, "be", $.expr, "minus", $.expr),
+      seq("print", $.expr),
+      seq("label", $.iden),
+      seq("goto",  $.iden),
+      seq("if", $.compare, "goto", $.iden),
+      seq("if", $.compare, "call", $.iden),
+      seq("push", $.expr),
+      seq("pull", $.expr),
+      seq("call", $.iden),
+      "return",
+      seq("write", $.expr, "into", $.expr),
+      seq("read",  $.var,  "from", $.expr),
+      seq("output", "newline"),
+      seq("output", $.expr),
+      seq("input",  $.var),
+      seq("allocate", $.number, "words", "for", $.var),
+      seq("string", $.string, "into", $.var),
+    ),
+    compare: $ => seq($.expr, choice(
+      "equal", "unequal", "greater", "lesser"
+    ), $.expr),
+    expr: $ => choice($.var, $.number),
+    string: $ => seq('"', /[^"]*/, '"'),
+    var: $ => $.iden,
+    iden: $ => /.+/,
+    number: $ => /[0-9]+/,
+
   }
 });
