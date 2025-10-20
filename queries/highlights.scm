7b169e555d932590b331cba475199f6dab9cb617
S1mon <jaforsi.kozak@gmail.com>
2025-10-20 05:25:42 +0200

    wrote highlights

---
+++ queries/highlights.scm
@@ -0,0 +1,18 @@
+
+(comment)   @comment
+(number)    @number
+(string)    @string
+(var)       @type
+(statement) @keyword
+
+"use"   @include
+
+
+(compare) @operator
+"plus"    @operator
+"minus"   @operator
+
+
+
+
+
